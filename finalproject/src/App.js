543ea26c5dbf29ab5dbc6a9ce34f3df540bc0aa5
Vedaste MUGABO <125447762+MugaboVedaste@users.noreply.github.com>
2024-12-04 09:07:25 +0200

    Update App.js
    another form added

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { ParentComponent, Counter, TodoList, LiveTime } from './components/MemoComponents';
-import { LecturerForm, StudentForm } from './components/Forms';
+import { LecturerForm, StudentForm, ModuleForm, BookForm, DriverForm } from './components/Forms';
 
 function App() {
   return (
@@ -15,6 +15,9 @@ function App() {
             <li><Link to="/time">Live Time</Link></li>
             <li><Link to="/lecturer">Lecturer Form</Link></li>
             <li><Link to="/student">Student Form</Link></li>
+            <li><Link to="/driver">Driver Form</Link></li>
+            <li><Link to="/book">Book Form</Link></li>
+            <li><Link to="/module">Module Form</Link></li>
           </ul>
         </nav>
 
@@ -25,6 +28,9 @@ function App() {
           <Route path="/time" element={<LiveTime />} />
           <Route path="/lecturer" element={<LecturerForm />} />
           <Route path="/student" element={<StudentForm />} />
+          <Route path="/driver" element={<DriverForm />} />
+          <Route path="/book" element={<BookForm />} />
+          <Route path="/module" element={<ModuleForm />} />
         </Routes>
       </div>
     </Router>
